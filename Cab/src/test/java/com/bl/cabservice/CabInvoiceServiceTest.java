b867aefff4e58ad1dcddc9a20db0a1f7a634bde0
sid-thakur47 <sidthakur258@gmail.com>
2020-05-17 21:43:10 +0530

    ADDED:Test cases for premium rides

--- Cab/src/test/java/com/bl/cabservice/CabInvoiceServiceTest.java
+++ Cab/src/test/java/com/bl/cabservice/CabInvoiceServiceTest.java
@@ -1,40 +1,41 @@
 package com.bl.cabservice;
 
+import com.bl.cabservice.constant.Constant;
+import com.bl.cabservice.service.CabInvoiceService;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CabInvoiceServiceTest {
+public class CabInvoiceServiceTest implements Constant {
     CabInvoiceService cabInvoiceService = new CabInvoiceService();
 
     @Test
-    public void givenDistanceANdTime_ShouldReturnFare() {
-
+    public void givenDistanceAndTimeFor_NormalCab_WhenProper_ShouldReturnFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = cabInvoiceService.calculateFare( distance, time );
+        double fare = cabInvoiceService.calculateFare( distance, time,NORMAL );
         Assert.assertEquals( 25, fare, 0.0 );
     }
     @Test
-    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+    public void givenLessDistanceOrTimeForNormalCab_WhenProper_ShouldReturnMinimumFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = cabInvoiceService.calculateFare( distance, time );
+        double fare = cabInvoiceService.calculateFare( distance, time ,NORMAL);
         Assert.assertEquals( 5.0, fare, 0.0 );
     }
     @Test
-    public void givenMultipleRides_ShouldReturnFare() {
+    public void givenMultipleRidesFor_NormalCab_WhenProper_ShouldReturnInvoice() {
         Ride[] rides = {
                 new Ride( 0.1, 1 ),
                 new Ride( 2.0, 5 ),
                 new Ride( 3.0, 5 )
         };
-        EnhancedInvoice enhancedInvoice = cabInvoiceService.calculateFare( rides );
+        EnhancedInvoice enhancedInvoice = cabInvoiceService.calculateFare( rides,NORMAL );
         EnhancedInvoice invoice = new EnhancedInvoice( 3, 65.0 );
         Assert.assertEquals( invoice, enhancedInvoice );
     }
 
     @Test
-    public void givenMultipleRidesFromRepository_ShouldReturnFare() {
+    public void givenMultipleRidesWithUserId_ForNormalCab_WhenProper_ShouldReturnInvoice() {
         String userId="sid";
         Ride[] rides = {
                 new Ride( 0.1, 1 ),
@@ -42,8 +43,49 @@ public class CabInvoiceServiceTest {
                 new Ride( 3.0, 5 )
         };
         cabInvoiceService.addRidesToRepository( userId,rides );
-        EnhancedInvoice enhancedInvoice = cabInvoiceService.getInVoiceSummary( userId );
-        EnhancedInvoice invoice = new EnhancedInvoice( 3, 65.0 );
+        EnhancedInvoice enhancedInvoice = cabInvoiceService.getInVoiceSummary( userId ,NORMAL);
+        EnhancedInvoice invoice = new EnhancedInvoice( 3, 65.0);
+        Assert.assertEquals( invoice, enhancedInvoice );
+    }
+
+    @Test
+    public void givenDistanceAndTime_ForPremiumCab_WhenProper_ShouldReturnInvoice() {
+
+        double distance = 2.0;
+        int time = 5;
+        double fare = cabInvoiceService.calculateFare( distance, time,PREMIUM);
+        Assert.assertEquals( 40, fare, 0.0 );
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ForPremiumCab_WhenProper_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = cabInvoiceService.calculateFare( distance, time ,PREMIUM);
+        Assert.assertEquals( 20, fare, 0.0 );
+    }
+    @Test
+    public void givenMultipleRidesOf_PremiumCab_WhenProper_ShouldReturnInvoice() {
+        Ride[] rides = {
+                new Ride( 0.1, 1 ),
+                new Ride( 2.0, 5 ),
+                new Ride( 3.0, 5 )
+        };
+        EnhancedInvoice enhancedInvoice = cabInvoiceService.calculateFare( rides,PREMIUM);
+        EnhancedInvoice invoice = new EnhancedInvoice( 3, 115.0 );
+        Assert.assertEquals( invoice, enhancedInvoice );
+    }
+    @Test
+    public void givenMultipleRidesWithUserId_ForPremiumCab_WhenProper_ShouldReturnInvoice() {
+        String userId="123";
+        Ride[] rides = {
+                new Ride( 0.1, 1 ),
+                new Ride( 2.0, 5 ),
+                new Ride( 3.0, 5 )
+        };
+        cabInvoiceService.addRidesToRepository( userId,rides);
+        EnhancedInvoice enhancedInvoice = cabInvoiceService.getInVoiceSummary( userId,PREMIUM);
+        EnhancedInvoice invoice = new EnhancedInvoice( 3, 115.0 );
         Assert.assertEquals( invoice, enhancedInvoice );
     }
 }
